4f0dcc159af09b251431c86358efaf25f0c1c066
kazk <kazk.dev@gmail.com>
2023-02-15 22:39:55 -0800

    Update hspec-formatters-codewars to v0.3.0

--- workspace/test/Main.hs
+++ workspace/test/Main.hs
@@ -1,9 +1,18 @@
-module Main where
-
-import Test.Hspec.Runner
-import Test.Hspec.Formatters.Codewars (codewars)
+module Main (main) where
 
 import qualified Spec
+import System.Exit
+import Test.Hspec.Core.Formatters.V2 (formatException, formatterToFormat)
+import Test.Hspec.Core.Util (safeTry)
+import Test.Hspec.Formatters.Codewars (escapeLF, newFormatter)
+import Test.Hspec.Runner
 
 main :: IO ()
-main = hspecWith defaultConfig {configFormatter = Just codewars} Spec.spec
+main = do
+  codewars <- newFormatter
+  summary <- safeTry $ runSpec Spec.spec defaultConfig {configFormat = Just $ formatterToFormat codewars}
+  case summary of
+    Left ex -> do
+      putStrLn $ "\n<ERROR::>Test suite crashed<:LF:>" ++ (escapeLF $ formatException ex)
+      exitFailure
+    Right s -> evaluateSummary s
